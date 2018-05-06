ff74cb4ec89fc9d31af8f50fbd51bc42f7daff5f
LalaCat <scott.si@hotmailcom>
2018-05-06 22:58:11 +0800

    增加defer-6，yield

--- test/twisted/defer-6-yeild.py
+++ test/twisted/defer-6-yeild.py
@@ -1,41 +1,69 @@
 from twisted.internet.defer import inlineCallbacks,Deferred,returnValue
+from twisted.internet import reactor,defer,task
+import requests
+from lxml import etree
 
-from twisted.internet import reactor,defer
+header = { 'User-Agent' :'MMozilla/5.0 (Windows NT 6.1; WOW64; rv:31.0) Gecko/20100101 Firefox/31.0'
+            ,'content-type':"application/json"}
+
+def request(url):
+    result = requests.get(url,headers=header)
+    return result
 
 
-def loadRemoteData(callback):
+def read_url(request):
+    #print(result.text)
+    result = etree.HTML(request.text)
+    return result
+
+
+@inlineCallbacks
+def time_wasted_wrapper(job_id):
     import time
-    time.sleep(1)
-    callback(1)
+    t1 = time.time()
+    print('begin time-wasted job '+str(job_id))
+    #result = requests.get("https://www.smzdm.com/", headers=header)
+    try:
+      result = yield request("https://www.baidu.com/")
+    except Exception as e :
+        print(e)
+    print('time-wasted job '+str(job_id)+' done!')
+    t2 = time.time()
+    print(t2-t1)
+    print(result)
 
+    r_s = etree.HTML(result.text)
+    ul = r_s.xpath("body/div[@id='content']/div/div[@id='feed-wrap']/div"
+                      "/div[@class='feed-main-con']/ul[@id='feed-main-list']")
+    #produce_list = ul[0].xpath('./li')
+    print(len(ul))
+    t3 = time.time()
+    print(t3-t2)
 
-def loadRemoteData2(callback):
-    import time
-    time.sleep(1)
-    callback(2)
+def all_jobs_done(result):
+    print(str(result))
+    print('all jobs are done!')
+    reactor.stop()
 
+def print_result1(result):
+    print("print 1")
+    for r in result:
+        print(r.xpath('./h5/a/@href'))
+    return
 
-@defer.inlineCallbacks
-def getRemoteData():
-    d1 = defer.Deferred()
-    reactor.callInThread(loadRemoteData, d1.callback)
-    r1 = yield d1
-
-    d2 = defer.Deferred()
-    reactor.callInThread(loadRemoteData2, d2.callback)
-    r2 = yield d2
-
-    returnValue(r1 + r2)
-
-
-def getResult(v):
-    print
-    "result=", v
-
+def install_jobs():
+    job_list = list()
+    for i in range(1):
+        job = time_wasted_wrapper(i)
+        job_list.append(job)
+    deferred_list = defer.DeferredList(job_list)
+    deferred_list.addCallback(print_result1)
+    deferred_list.addCallback(all_jobs_done)
 
 if __name__ == '__main__':
-    d = getRemoteData()
-    d.addCallback(getResult)
-
-    reactor.callLater(4, reactor.stop);
+    job = time_wasted_wrapper(1)
+    #job.addCallback(print_result1)
+    job.__next__()
+    job.addCallback(all_jobs_done)
+    print('all job have started!')
     reactor.run()
