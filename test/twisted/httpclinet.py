b2f3a42edb11d2e788dec5140b2448045c887389
LalaCat <scott.si@hotmailcom>
2018-05-17 22:45:49 +0800

    修改文件

--- test/twisted/httpclinet.py
+++ test/twisted/httpclinet.py
@@ -3,14 +3,22 @@ from twisted.web.client import Agent
 from twisted.web.http_headers import Headers
 from twisted.internet import reactor, task, defer
 
+headers=Headers({'User-Agent':['Twisted WebBot'],
+                 'Content-Type':['text/x-greeting']})
+
 def hello(name):
     print("Hello world!===>" + name + '===>' + str(int(time.time())))
+
+
 
 @defer.inlineCallbacks
 def request_web():
     agent = Agent(reactor)
+    url = b'https://www.baidu.com'
+    print(type(url))
     try:
-        result = yield agent.request('GET', 'http://www.baidu.com', Headers({'User-Agent': ['Twisted Web Client Example']}), None)
+        result = yield agent.request('GET', url,headers, None)
+
     except Exception as e:
         print(e)
         return
@@ -22,5 +30,5 @@ reactor.callWhenRunning(hello, 'yudahai')
 reactor.callLater(1, request_web)
 
 reactor.callLater(3, hello, 'yuyue')
-
+reactor.callLater(3,reactor.stop)
 reactor.run()
