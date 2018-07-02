87c770154f86eec88479296425afea25c1174f2d
scott <scott.si@hotmail.com>
2018-07-03 01:55:16 +0800

    实现了innerdefer中DeferList的功能 添加了Url中带有中文字符的问题

--- test/twisted/defer/chaindefer-4.py
+++ test/twisted/defer/chaindefer-4.py
@@ -32,26 +32,26 @@ class Spider(object):
 
     def __init__(self):
         self.Flag = True
+        self.url = "http://httpbin.org/get"
     def start_requests(self):
-        start_url = ["https://www.baidu.com", "https://www.bing.com", ]
+        start_url = ["https://www.baidu.com", "https://www.bing.com", "https://www.sogou.com/"]
         for url in start_url:
             yield Requset(url, self.parse)
 
     def parse(self, response):
         print("---------response--------->")
-        #a.callback(None)
+
         d2 = getPage(b"http://httpbin.org/get")
+        d2.addCallback(self.inner_callback)
+        try:
+            print("str:", d2.result.value)
+        except Exception as e :
+            print(e)
         return d2
 
-    def finish(self,content):
-        print("finish")
-        self.Flag = False
-
-
-    def print_child_web(self,content):
-        print("print_child_web:",content)
-        #print(content)
-        return
+    def inner_callback(self,content):
+        print("Spider_inner:",content)
+        return "callback %s" %self.url
 
 import queue
 
@@ -66,47 +66,56 @@ class Engine(object):
         self._close = None
 
     def get_response_callback(self, content, request):
-        #print("get_response_callback")
-        self.crawlling.remove(request)
-        #print(rep.content)
 
         result = request.parse(content)
-        result.addCallback(self.inner_print)
+
+        result.addCallback(self.engine_inner)
+        print("get_response_callback end")
         return result
 
 
     def _next_request(self,spider):
-        print("spider name:",spider.name)
-        print('----->request', Q.qsize())
+        print(str(Q.qsize()),str(len(self.crawlling)))
+
         if Q.qsize() == 0 and len(self.crawlling) == 0:
             self._close.callback(None)
-            return
+            return None
+        print("spider name:",spider.name)
 
-        while len(self.crawlling) < self.max:
+        while Q.qsize() > 0 :
             try:
                 req = Q.get(block=False)
-                self.crawlling.append(req)
                 # getPage返回的是一个defer对象
                 d = getPage(req.url.encode("utf-8"))
-                print("d",d)
 
                 # 当页面下载完之后，会立即调用回调函数get_response_callback
-                d.addCallback(self.outer_print)
+                d.addCallback(self.engine_outer,req.url,1)
                 d.addCallback(self.get_response_callback, req)
-                d.addCallback(self.outer_print)
+                d.addCallback(self.engine_outer,req.url,2)
+                self.crawlling.append(req.url)
+                d.addCallback(self.finish,req.url)
+
                 d.addCallback(lambda _:reactor.callLater(0,self._next_request,spider))
             except Exception as e:
                 print("Exception",e)
                 break
-               #print(e)
+            print("循环结束")
 
-    def outer_print(self,content):
-        print("outer_callback")
-        return 1
 
-    def inner_print(self,content):
-        print("innner_callback", content)
-        return 2
+
+
+    def engine_outer(self,content,url,num):
+        print("engine_outer： 返回%s的内容 : %d"%(url,num))
+        return "engine_outer %s" %url
+
+    def engine_inner(self,content):
+        print("engine_inner", content)
+        return "engine_inner"
+    def finish(self,content,url):
+        print(content)
+        self.crawlling.remove(url)
+        return None
+
 
     @inlineCallbacks
     def crwal(self, spider):
@@ -137,6 +146,6 @@ d = engine.crwal(spider)
 #_active.add(d)
 
 dd = DeferredList([d,])
-#dd.addCallback(lambda _:reactor.stop())
+dd.addCallback(lambda _:reactor.stop())
 
 reactor.run()
--- test/twisted/test-getPage-qyl163.py
+++ test/twisted/test-getPage-qyl163.py
@@ -1,36 +1,48 @@
+
 from twisted.web.client import getPage
 from twisted.internet import reactor,defer
-from twisted.internet.defer import inlineCallbacks,Deferred,returnValue
+from twisted.internet.defer import inlineCallbacks,Deferred,returnValue,DeferredList
 #from test.public_api.web import get_need_datas,print_result
 import json,time
 from bs4 import BeautifulSoup
-
-url = 'https://www.smzdm.com/homepage/json_more?p='
+from twisted.protocols.sip import URL
+from twisted.python.urlpath import  URLPath
 headers = {'User-Agent' :'MMozilla/5.0 (Windows NT 6.1; WOW64; rv:31.0) Gecko/20100101 Firefox/31.0'
             ,'content-type':"application/json"}
 
 
-def print_qyl163_content(lis,url):
-    print("print:%s"%url)
+def print_qyl163_content(lis,u):
+    urls = list()
+    print("print:%s"%u)
     for l in lis:
         result = dict()
-        href_temp = l.a.get("href")
-        result["href"] = "http://www.qyl63.com" + href_temp
-        result["title"] = l.a.get("titile")
-        result["img"] = l.a.div.img.get("src")
-        #print(result["href"])
-        child_web = second_defer("http://httpbin.org/get")
- #       child_web.addCallBack(print_web)
+        try:
+            href_temp = l.a.get("href")
+            result["href"] = "http://www.qyl63.com" + href_temp
+            result["title"] = l.a.get("titile")
+            result["img"] = l.a.div.img.get("src")
+            u  =  result["href"]
 
+            #解决url中带有中文字符
+            us = URLPath.fromString(u)
+            child_web = getPage(us.path)
 
+            #child_web.addCallBack(add_video_url,result)
+            urls.append(child_web)
+        except Exception as e :
+            print(e)
+    return DeferredList(urls)
 
-@defer.inlineCallbacks
-def second_defer(url):
-    print('first callback')
-   #print(url)
-    result = yield getPage(url.encode("utf-8"))
-    print("result",result)
-    returnValue(result)
+def add_video_url(child_page,result):
+    print("add url")
+    try:
+        bs_obj = BeautifulSoup(child_page, "html.parser")
+        video = bs_obj.find("video", "player_html5_api")
+        url = video.get("src")
+        result["video_url"] = url
+    except Exception as e :
+        print(e)
+    return None
 
 
 def get_qyl163_content(content):
@@ -45,29 +57,33 @@ def get_qyl163_content(content):
 
     return lis
 
-def get_qyl_video(child_page):
-    bs_obj = BeautifulSoup(child_page, "html.parser")
-    video = bs_obj.find("video", "player_html5_api")
-    returnValue(video)
+
 
 def print_web(context):
     print("print_web")
     print(context)
 
+
+def finish(context):
+    print("finish")
+    print(context)
+
+
+
 @inlineCallbacks
 def read_url(url):
     d = getPage(url.encode('utf-8'))
+    try:
+        d.addCallbacks(get_qyl163_content)
+        d.addCallback(print_qyl163_content,url)
+        d.addCallback(finish)
+    except Exception as e :
+        print(e)
 
-    #d.addCallback(print_web)
-    d.addCallbacks(get_qyl163_content,print_web)
-    #d.addErrorback(print_web)
-    d.addCallback(print_qyl163_content,url)
 
     yield d
 
-@inlineCallbacks
-def read_child_url(url):
-    pass
+
 
 if __name__ == '__main__':
     start = time.clock()
