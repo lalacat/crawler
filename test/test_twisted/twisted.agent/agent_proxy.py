e5bbbd4b5a15abc8d7e26cf0472d151da809492b
LalaCat <scott.si@hotmailcom>
2018-12-11 13:14:07 +0800

    proxy

--- test/test_twisted/twisted.agent/agent_proxy.py
+++ test/test_twisted/twisted.agent/agent_proxy.py
@@ -1,26 +1,32 @@
+from twisted.internet.ssl import ClientContextFactory
 from twisted.python.log import err
 from twisted.web.client import ProxyAgent
 from twisted.internet import reactor, defer
-from twisted.internet.endpoints import TCP4ClientEndpoint
+from twisted.internet.endpoints import TCP4ClientEndpoint,SSL4ClientEndpoint
 
 def display(response,url):
     print("Received response: %s"%url)
     print(response)
+class WebClientContextFactory(ClientContextFactory):
+    '''
+    用来实现https网页的访问验证
+    '''
+    def getContext(self,hostname, port):
+        return ClientContextFactory.getContext(self)
 
 
-urls = ["124.205.155.154","112.95.207.240","27.191.234.69","45.63.87.141"]
-prots = [9090,8888,9999,3128]
+contextFactory = WebClientContextFactory()
+
+host = "149.28.192.96"
+port = 527
 def main():
-    agents = []
-    for url,prot in zip(urls,prots):
-        endpoint = TCP4ClientEndpoint(reactor,url, prot)
-        agent = ProxyAgent(endpoint)
-        d = agent.request(b"GET", b"https://www.smzdm.com")
-        d.addCallback(display,url)
-        d.addErrback(err)
-        agents.append(d)
-    dd = defer.DeferredList(agents)
-    dd.addCallback(lambda ignored: reactor.stop())
+    # endpoint = SSL4ClientEndpoint(reactor,host, port,contextFactory)
+    endpoint = TCP4ClientEndpoint(reactor, 'localhost', 443)
+    agent = ProxyAgent(endpoint)
+    d = agent.request(b"GET", b"https://baidu.com/")
+    d.addCallback(display,host)
+    d.addErrback(err)
+    d.addCallback(lambda ignored: reactor.stop())
     reactor.run()
 
 if __name__ == "__main__":
